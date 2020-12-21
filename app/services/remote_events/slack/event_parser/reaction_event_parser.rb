81da778b089df9317fb5038b02e2a894cc7c7dc0
Jeffrey Goldsmith <jeffgold12@gmail.com>
2020-12-20 22:49:43 -0500

    Create reaction event parser

---
+++ app/services/remote_events/slack/event_parser/reaction_event_parser.rb
@@ -0,0 +1,36 @@
+# typed: strict
+# frozen_string_literal: true
+
+module RemoteEvents
+  module Slack
+    module EventParser
+      class ReactionEventParser < BaseEventParser
+        extend(T::Sig)
+
+        SUPPORTED_SLACK_EVENT_TYPES = T.let([
+            ::Slack::RemoteEvent::Type::ReactionAdded,
+            ::Slack::RemoteEvent::Type::ReactionRemoved,
+          ].freeze,
+          T::Array[::Slack::RemoteEvent::Type]
+        )
+
+        sig { override.returns(::Slack::RemoteEvent) }
+        def parse
+          metadata = {
+            user: @event[:user],
+            reaction: @event[:reaction],
+            message_channel: @event[:item][:channel],
+            message_timestamp: @event[:item][:ts]
+          }
+
+          ::Slack::RemoteEvent.new(type: @event_type, metadata: metadata)
+        end
+
+        sig { override.params(event_type: ::Slack::RemoteEvent::Type).returns(T::Boolean) }
+        def self.accepts_event_type?(event_type:)
+          SUPPORTED_SLACK_EVENT_TYPES.include?(event_type)
+        end
+      end
+    end
+  end
+end
