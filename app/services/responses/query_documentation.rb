f4fb7127c0ba8c83ff60127fe173ef09e377ac36
Jeffrey Goldsmith <jeffgold12@gmail.com>
2020-12-23 17:29:35 -0500

    Create tests for QueryDocumentation response

--- app/services/responses/query_documentation.rb
+++ app/services/responses/query_documentation.rb
@@ -30,16 +30,17 @@ module Responses
       # Find all documentation local to a given channel that matches the input string.
       # Additionally, order results by most recent to prepare them to be displayed to the user.
       results = Documentation
-        .where(slack_channel_id: channel_id)
-        .where("text LIKE (?)", "%#{query}%")
-        .order(updated_at: :desc)
+        .where(slack_channel_id: @channel_id)
+        .where("text LIKE (?)", "%#{@query}%")
+        .order(created_at: :desc)
+        .to_a
 
       # Send the formatted result to the user.
       # If there are no results, let them know.
       # Otherwise, give a formatted list of the documentation found through the above query.
       Slack::Client.current.chat_postMessage(
-        channel: channel_id,
-        text: results.empty? "No documentation matches your search :(" : format_results(results),
+        channel: @channel_id,
+        text: results.empty? ? "No documentation matches your search :(" : format_results(results),
         as_user: true
       )
     end
@@ -55,8 +56,8 @@ module Responses
       # Append each documentation, formatted and separated by a newline
       result_string += results.map do |documentation|
         # Fetch relevant documentation information from slack in order to format properly
-        conversation_info = ::Slack::Client.conversations_info(channel: documentation.slack_channel_id)
-        user_info = ::Slack::Client.users_info(user: documentation.slack_user_id)
+        conversation_info = ::Slack::Client.current.conversations_info(channel: documentation.slack_channel_id)
+        user_info = ::Slack::Client.current.users_info(user: documentation.slack_user_id)
 
         documentation.format(
           channel_name: conversation_info[:channel][:name],
--- test/services/responses/query_documentation_test.rb
+++ test/services/responses/query_documentation_test.rb
@@ -4,51 +4,95 @@
 require 'test_helper'
 
 module Responses
-  class CreateDocumentationTest < ActiveSupport::TestCase
-    test "#call persists a new documentation with the correct details" do
-      expected_documentation = build(:documentation)
+  class QueryDocumentationTest < ActiveSupport::TestCase
+    include SlackTestHelper
 
-      mocked_client = mock
-      Slack::Client.expects(:current).returns(mocked_client)
-      mocked_client.expects(:reactions_add)
+    setup do
+      @channel_id = "test_channel_id"
+      @expected_documentation = []
+      @mocked_client = mock
+      Slack::Client.expects(:current).returns(@mocked_client).at_least_once
 
-      assert_difference -> { Documentation.count }, 1 do
-        call_service(
-          text: expected_documentation.text,
-          user_id: expected_documentation.slack_user_id,
-          channel_id: expected_documentation.slack_channel_id,
-          timestamp: expected_documentation.slack_timestamp
-        )
+      3.times do |i|
+        travel_to(Time.utc(2020, 1, i + 1)) do
+          @expected_documentation << create(
+            :documentation,
+            text: "test #{i}",
+            slack_channel_id: @channel_id
+          )
+        end
       end
-
-      documentation = Documentation.first
-
-      assert_equal expected_documentation.text, documentation.text
-      assert_equal expected_documentation.slack_user_id, documentation.slack_user_id
-      assert_equal expected_documentation.slack_channel_id, documentation.slack_channel_id
-      assert_equal expected_documentation.slack_timestamp, documentation.slack_timestamp
     end
 
-    test "#call reacts to the documentation message with the fyi saved emoji" do
-      expected_documentation = build(:documentation)
+    test "#call returns a list of formatted results to the user" do
+      refuted_documentation = create(:documentation, text: "refuted")
 
-      mocked_client = mock
-      Slack::Client.expects(:current).returns(mocked_client)
-      mocked_client.expects(:reactions_add).with(
-        channel: expected_documentation.slack_channel_id,
-        name: Rails.application.config.fyi_saved_emoji,
-        timestamp: expected_documentation.slack_timestamp
-      )
+      stub_formatting
+      @mocked_client
+        .expects(:chat_postMessage)
+        .with do |args|
+          @expected_documentation.each do |documentation|
+            assert_match /#{documentation.text}/, args[:text]
+          end
+
+          refute_match /#{refuted_documentation.text}/, args[:text]
+        end
 
       call_service(
-        text: expected_documentation.text,
-        user_id: expected_documentation.slack_user_id,
-        channel_id: expected_documentation.slack_channel_id,
-        timestamp: expected_documentation.slack_timestamp
+        query: "test",
+        channel_id: @channel_id
+      )
+    end
+
+    test "#call returns a list of results sorted by most recent" do
+      stub_formatting
+      @mocked_client
+        .expects(:chat_postMessage)
+        .with do |args|
+          sorted_result_regexp = /.*#{@expected_documentation.reverse.map(&:text).join("(.|\\s)*")}.*/
+          assert_match sorted_result_regexp, args[:text]
+        end
+
+      call_service(
+        query: "test",
+        channel_id: @channel_id
+      )
+    end
+
+    test "#call returns a 404 message if no results are found" do
+      @mocked_client
+        .expects(:chat_postMessage)
+        .with do |args|
+          assert_match /No documentation matches your search :\(/, args[:text]
+        end
+
+      call_service(
+        query: "no match",
+        channel_id: @channel_id
       )
     end
 
     private
+
+    def stub_formatting
+      @mocked_client
+        .expects(:conversations_info)
+        .returns({
+          channel: {
+            name: "test channel name"
+          }
+        })
+        .at_least_once
+
+      @mocked_client
+        .expects(:users_info)
+        .returns({
+          user: {
+            name: "test user name"
+          }
+        })
+        .at_least_once
+    end
 
     def call_service(query:, channel_id:)
       QueryDocumentation.new(
