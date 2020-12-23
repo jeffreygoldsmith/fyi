1e955c5db7dec97396b9043beeefc4500647cf38
Jeffrey Goldsmith <jeffgold12@gmail.com>
2020-12-23 14:42:51 -0500

    Create formatting method on Documentation model

--- app/models/documentation.rb
+++ app/models/documentation.rb
@@ -2,4 +2,16 @@
 # frozen_string_literal: true
 class Documentation < ApplicationRecord
   self.table_name = "documentation"
+
+  FORMATTED_DOCUMENTATION = "[%s %s %s] %s"
+  FORMATTED_TIME = "%a %b %e %T %Z %Y"
+
+  def format(channel_name:, user_name:)
+    FORMATTED_DOCUMENTATION % [
+      updated_at.strftime(FORMATTED_TIME),
+      "#" + channel_name,
+      "@" + user_name,
+      text,
+    ]
+  end
 end
