6cfdb0a11b432a479013ea1a761a9fd3465ae2cc
Jeffrey Goldsmith <jeffgold12@gmail.com>
2020-12-22 23:30:42 -0500

    Add fyi emoji constants in application configuration

--- config/application.rb
+++ config/application.rb
@@ -41,5 +41,8 @@ module Fyi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.fyi_emoji = "fyi"
+    config.fyi_saved_emoji = "fyi-saved"
   end
 end
