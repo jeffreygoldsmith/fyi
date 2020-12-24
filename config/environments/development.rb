f8c21dfde0af35489da434384d5c317a33934ade
Jeffrey Goldsmith <jeffgold12@gmail.com>
2020-12-24 01:17:52 -0500

    Add whitelist for ngrok

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # A workaround to whitelist ngrok during development
+  config.hosts << /[a-z0-9]+\.ngrok\.io/
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
